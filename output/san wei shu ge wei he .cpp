7cf6fdf6f41acf3bd6864868b4d8b1f285d6563b
226211 <508800596@qq.com>
2023-03-30 22:14:35 +0800

    l

--- output/san wei shu ge wei he .cpp
+++ output/san wei shu ge wei he .cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 int main()
 {
-    int n, a[100],b;
+    int n, a[100];
     cin >> n;
     a[1] = n / 100;
     a[2] = (n % 100) / 10;
     a[3] = n % 10;
     cout << (a[1] + a[2] + a[3]);
+    system("pause");
 }
