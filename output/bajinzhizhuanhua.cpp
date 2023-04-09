7a99302689dc250fe3bc92a479de904c340d5992
226211 <508800596@qq.com>
2023-04-09 18:05:03 +0800

    l

---
+++ output/bajinzhizhuanhua.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+class transform
+{
+    public:
+        int a,b,c;
+        int DtoO;
+        {
+            
+        }
+        
+        
+};
